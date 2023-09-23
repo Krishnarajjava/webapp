00f0385f2d12908591c3080a4b2f0a846e65ef4c
Krishnarajjava <bkrish058@gmail.com>
2023-09-23 12:52:43 +0530

    commit

---
+++ src/main/java/com/myWebApp/controller/WebController.java
@@ -0,0 +1,14 @@
+package com.myWebApp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebController {
+	@GetMapping(value="/name")
+	public String getName() {
+		return "Krishnaraj";
+	}
+	
+
+}
---
+++ src/test/java/com/myWebApp/Mywebapp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mywebapp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
